90583e543cb8c1fbdf00f329124c19827024e82a
EvgenNik2023 <140894608+EvgenNik2023@users.noreply.github.com>
2023-08-20 09:19:11 +0300

    Update Program.cs

--- example002_HelloUser/Program.cs
+++ example002_HelloUser/Program.cs
@@ -4,3 +4,5 @@ string username = Console.ReadLine();
 Console.Write("Привет, ");
 Console.WriteLine(username);
 Console.WriteLine("Как дела?");
+string answere = Console.ReadLine();
+Console.WriteLine(answere)
