2d2664009315d0ec31410eec2fda1bcc0eda4847
Evgen <makeevgen1975@gmail.com>
2023-08-20 18:41:57 +0300

    добавил example003_sum

--- example003_sum/Program.cs
+++ example003_sum/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = 4;
+int numberB = 5;
+int sum = numberA + numberB;
+Console.WriteLine(sum);
